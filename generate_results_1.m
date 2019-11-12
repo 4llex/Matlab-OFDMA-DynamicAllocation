6c6cb9126d94c313c62f446244f9398c9667a308
4llex <alexr@gec.inatel.br>
2019-11-12 17:13:14 -0300

    created scripts to generate results

---
+++ generate_results_1.m
@@ -0,0 +1,56 @@
+% Script para gerar os resultados das simulações 
+% das numerologias 1 e 3 utilizando as tecnicas de alocação
+% Estatica e Dinamica para Máxima Vazão
+
+%% Loading Results
+
+% Static Allocation Num 1
+SimData=load('staticAllocation_num1.mat');
+EA1 = SimData.Sim.DataSNR;
+EA2 = SimData.Sim.DataBPRB;  
+
+% Static Allocation Num 3
+SimData=load('staticAllocation_num3.mat');
+EA3 = SimData.Sim.DataSNR;
+EA4 = SimData.Sim.DataBPRB;
+
+% Dynamic Allocation Num 1
+DynamicData=load('dynamicMaxVazao_num1.mat');
+DA1 = DynamicData.Dynamic.DataSNR;
+DA2 = DynamicData.Dynamic.DataBPRB;
+
+% Dynamic Allocation Num 3
+DynamicData=load('dynamicMaxVazao_num3.mat');
+DA3 = DynamicData.Dynamic.DataSNR;
+DA4 = DynamicData.Dynamic.DataBPRB;
+
+
+%% Gera graficos de Bits/SNR
+figure;
+plot(EA1, EA2,'-.rd');
+xlabel('SNR [dB]'); 
+ylabel('Bits/RB'); 
+grid on;
+grid minor;
+
+hold on;
+plot(EA3,EA4,'-.rs',...
+    'LineWidth',1,...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.49 1 .63],...
+    'MarkerSize',6)
+%plot(EA3, EA4, ':rs');
+
+hold on;
+plot(DA1, DA2, '-.bo');
+
+hold on;
+plot(DA3,DA4,'-.bv',...
+    'LineWidth',1,...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.49 1 .63],...
+    'MarkerSize',6)
+%plot(DA3, DA4, ':bv');
+
+lgd = legend('Alocação Estática - Alta Vazão','Alocação Estática - IoT', 'Alocação Dinâmica - Alta Vazão', 'Alocação Dinâmica - IoT');
+lgd.FontSize = 10;
---
+++ generate_results_2.m
@@ -0,0 +1,74 @@
+% Script para gerar os resultados das simulações 
+% das numerologias 1 e 3 utilizando as tecnicas de alocação
+% Estatica e Dinamica para Máxima Vazão
+
+%% Loading Results
+
+% Static Allocation Num 1
+SimData=load('staticAllocation_num1.mat');
+EA1 = SimData.Sim.DataSNR;
+EA2 = SimData.Sim.DataBPRB;  
+
+% Static Allocation Num 3
+SimData=load('staticAllocation_num3.mat');
+EA3 = SimData.Sim.DataSNR;
+EA4 = SimData.Sim.DataBPRB;
+
+% Dynamic Allocation Num 1
+DynamicData=load('dynamicMaxVazao_num1.mat');
+DA1 = DynamicData.Dynamic.DataSNR;
+DA2 = DynamicData.Dynamic.DataBPRB;
+
+% Dynamic Allocation Num 3
+DynamicData=load('dynamicMaxVazao_num3.mat');
+DA3 = DynamicData.Dynamic.DataSNR;
+DA4 = DynamicData.Dynamic.DataBPRB;
+
+% AWM/MOM - Num 1
+DynamicMOM=load('dynamic_mom_tese_num1.mat');
+AWM1 = DynamicMOM.DynamicMOM.DataSNR;
+AWM2 = DynamicMOM.DynamicMOM.DataBPRB;
+
+% AWM/MOM - Num 3
+DynamicMOM=load('dynamic_mom_tese_num3.mat');
+AWM3 = DynamicMOM.DynamicMOM.DataSNR;
+AWM4 = DynamicMOM.DynamicMOM.DataBPRB;
+
+
+%% Gera graficos Alta Vazao
+figure;
+plot(EA1,EA2,'-.rs',...
+    'LineWidth',1,...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.49 1 .63],...
+    'MarkerSize',6)
+xlabel('SNR [dB]'); 
+ylabel('Bits/RB'); 
+grid on;
+grid minor;
+hold on;
+
+plot(DA1, DA2, '-.b*');
+
+plot(AWM1, AWM2, '-.ko');
+
+lgd0 = legend('Alocação Estática','Alocação Dinâmica', 'AWM-MOM');
+lgd0.FontSize = 10;
+%% Gera graficos IoT
+figure;
+plot(EA3,EA4,'-.rs',...
+    'LineWidth',1,...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor',[.49 1 .63],...
+    'MarkerSize',6)
+xlabel('SNR [dB]'); 
+ylabel('Bits/RB'); 
+grid on;
+grid minor;
+hold on;
+plot(DA3, DA4, '-.b*');
+
+plot(AWM3, AWM4, '-.ko');
+
+lgd = legend('Alocação Estática','Alocação Dinâmica', 'AWM-MOM');
+lgd.FontSize = 10;
